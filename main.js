5e02d8e4c81e9d2bb8fb1133c4c472ca83124756
Felix Brucker <felix@felixbrucker.com>
2021-06-03 15:42:57 +0200

    Add migrations for next satellite update

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ const db = require('./lib/service/db');
 const oAuth = require('./lib/service/oauth');
 const housekeeping = require('./lib/service/housekeeping');
 const rateService = require('./lib/service/rate-service');
+const migrations = require('./lib/migrations');
 
 (async () => {
   sentryService.init();
   await db.init();
+  await migrations.migrate();
   await oAuth.init();
   await housekeeping.init();
   await rateService.init();
